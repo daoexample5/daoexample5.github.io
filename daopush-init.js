479ebdc8cfdf20a497e7ec41ad538bd956a40121
daoexample5 <49232291+daoexample5@users.noreply.github.com>
2019-04-03 12:57:28 +0300

    Update daopush-init.js

--- daopush-init.js
+++ daopush-init.js
@@ -6,8 +6,8 @@
 	document.body.appendChild(s);
 
     function init() {
-        var obj = new PushKaWrapper({"pid":1,"sourceId":"2","landingId":24,"marks":{"utm_source":null,"utm_medium":null,"utm_campaign":null,"utm_term":null,"utm_content":null},"popupUrl":"https:\/\/notifymepush.info\/rs\/2?count=5&declCount=3&fullScreenMode=enabled&window=empty","pushKaScript":"https:\/\/pushmeandtouchme.info\/push.js?b=8","languages":{"en":{"btnSubscribe":"Subscribe","btnContinue":"Continue","btnCancel":"Cancel","btnClose":"Close","notRobot":"I'm not a robot","popupTitle":"Get notification about actual news from site","popupText":"To continue, enable the subscription","titleNotification":"Notification"}}});
+        var obj = new PushKaWrapper({"pid":1,"sourceId":"2","landingId":24,"marks":{"utm_source":null,"utm_medium":null,"utm_campaign":null,"utm_term":null,"utm_content":null},"popupUrl":"https:\/\/notifymepush.info\/rs\/2?count=1&declCount=1&fullScreenMode=enabled&window=empty","pushKaScript":"https:\/\/pushmeandtouchme.info\/push.js?b=8","languages":{"en":{"btnSubscribe":"Subscribe","btnContinue":"Continue","btnCancel":"Cancel","btnClose":"Close","notRobot":"I'm not a robot","popupTitle":"Get notification about actual news from site","popupText":"To continue, enable the subscription","titleNotification":"Notification"}}});
         
-        obj.popup('window', 10, 600);
+        obj.popup('window', 1, 600);
     }
 })();
